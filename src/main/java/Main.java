85d2e2ecdce3d7d2d8f1eddaa661714419f1c046
Beatriz Huberto <169067859+trizrto@users.noreply.github.com>
2024-06-17 12:30:24 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+  public static void main(String[] args) {
+    int dia = 27;
+    int mes = 6;
+      int ano = 2009;
+    String nome = "Beatriz";
+  System.out.println("O meu nome é: "+ nome + "  // Minha data e nascimento é:" + dia  + mes  + ano );
+    
+  }
+
+ 
+} 
